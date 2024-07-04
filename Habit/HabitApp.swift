c35f52b10bc94306dec716d3ad6a6cb85eab58c7
ismv00 <euigoorr@MacBook-Pro-de-Igor.local>
2024-07-03 21:42:50 -0400

    first-commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Igor S. Menezes on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
